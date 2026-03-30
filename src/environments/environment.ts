cc9a79d269cfc21652e50bc3f99f1c6e66e0810e
giomellios <melliosgiorgos3@gmail.com>
2026-03-30 17:10:57 +0300

    replace tmdbApiKey with placeholder for security

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  tmdbApiKey: "85204a8cc33baf447559fb6d51b18313",
+  tmdbApiKey: "YOUR_TMDB_API_KEY",
   tmdbApiBaseUrl: "https://api.themoviedb.org/3",
   tmdbImageBaseUrl: "https://image.tmdb.org/t/p",
 } as const;
